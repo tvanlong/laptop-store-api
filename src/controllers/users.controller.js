f432f3bc9de0fed69737343af49f0140dc4c6813
tvanlong <longvan1173@gmail.com>
2024-05-25 13:57:12 +0700

    feat: Add customer profile and password update endpoints

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import User from '~/models/user.model'
-import { userValid } from '~/validation/user.validation'
+import { changePasswordValid, profileCustomerValid, userValid } from '~/validation/user.validation'
 import bcryptjs from 'bcryptjs'
 
 export const getAllCustomers = async (req, res, next) => {
@@ -13,6 +13,23 @@ export const getAllCustomers = async (req, res, next) => {
     return res.status(200).json({
       message: 'Lấy danh sách khách hàng thành công',
       data: customers
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getCustomer = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const customer = await User.findById(id)
+    if (!customer) {
+      return res.status(404).json({ message: 'Không tìm thấy khách hàng' })
+    }
+
+    return res.status(200).json({
+      message: 'Lấy thông tin khách hàng thành công',
+      data: customer
     })
   } catch (error) {
     next(error)
@@ -72,6 +89,77 @@ export const createStaff = async (req, res, next) => {
       message: 'Tạo nhân viên mới thành công',
       data: userInfo
     })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const updateCustomer = async (req, res, next) => {
+  try {
+    const { error } = profileCustomerValid.validate(req.body, { abortEarly: false })
+    if (error) {
+      const errors = error.details.map((item) => item.message)
+      return res.status(400).json({ errors })
+    }
+
+    const { id } = req.params
+    const customer = await User.findById(id)
+    if (!customer) {
+      return res.status(404).json({ message: 'Không tìm thấy khách hàng' })
+    }
+
+    const { email } = req.body
+    const userExists = await User.find({ email })
+    if (userExists.length > 0) {
+      userExists.forEach((user) => {
+        if (user.email === email && user._id != id) {
+          return res.status(400).json({ message: 'Email đã tồn tại!' })
+        }
+      })
+    }
+
+    const updatedCustomer = await User.findByIdAndUpdate(
+      id,
+      {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone
+      },
+      { new: true }
+    )
+    const { password, ...userInfo } = updatedCustomer._doc
+    return res.status(200).json({
+      message: 'Cập nhật thông tin khách hàng thành công',
+      data: userInfo
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const changePassword = async (req, res, next) => {
+  try {
+    const { error } = changePasswordValid.validate(req.body, { abortEarly: false })
+    if (error) {
+      const errors = error.details.map((item) => item.message)
+      return res.status(400).json({ errors })
+    }
+
+    const { id } = req.params
+    const customer = await User.findById(id)
+    if (!customer) {
+      return res.status(404).json({ message: 'Không tìm thấy khách hàng' })
+    }
+
+    const { password } = req.body
+    const isMatch = await bcryptjs.compare(password, customer.password)
+    if (isMatch) {
+      return res.status(400).json({ message: 'Mật khẩu mới không được trùng với mật khẩu cũ' })
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10)
+    await User.findByIdAndUpdate(id, { password: hashedPassword }, { new: true })
+    return res.status(200).json({ message: 'Đổi mật khẩu thành công' })
   } catch (error) {
     next(error)
   }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,17 +1,26 @@
 import { Router } from 'express'
 import { checkPermission } from '~/middlewares/checkPermission'
 import {
+  changePassword,
   createStaff,
   deleteStaff,
   getAllCustomers,
   getAllStaffs,
+  getCustomer,
   getStaff,
+  updateCustomer,
   updateStaff
 } from '~/controllers/users.controller'
 
 const routerUser = Router()
 
+// Route for customer
 routerUser.get('/customers', checkPermission('admin'), getAllCustomers)
+routerUser.get('/customers/:id', getCustomer)
+routerUser.patch('/customers/:id', updateCustomer)
+routerUser.patch('/customers/:id/change-password', changePassword)
+
+// Route for staff
 routerUser.get('/staffs', checkPermission('admin'), getAllStaffs)
 routerUser.get('/staffs/:id', checkPermission('admin'), getStaff)
 routerUser.post('/staffs', checkPermission('admin'), createStaff)
