f87420cdf4e42615b2eb47c4a00b9e94311b26dd
tvanlong <longvan1173@gmail.com>
2024-05-28 17:20:22 +0700

    chore: Update setTokenIntoCookie function to handle different user roles

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,23 +2,54 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const setTokenIntoCookie = (res, accessToken, refreshToken, user) => {
-  const isProd = process.env.BUILD_MODE === 'prod'
-  const cookieOptions = {
-    httpOnly: isProd,
-    maxAge: 7 * 24 * 60 * 60 * 1000
-  }
-
-  if (isProd) {
-    cookieOptions.secure = true
-    cookieOptions.sameSite = 'none'
-  }
-
-  if (user.role === 'admin') {
-    res.cookie('access_token_admin', accessToken, cookieOptions)
-    res.cookie('refresh_token_admin', refreshToken, { ...cookieOptions, maxAge: 30 * 24 * 60 * 60 * 1000 })
-  } else if (user.role === 'member') {
-    res.cookie('access_token_member', accessToken, cookieOptions)
-    res.cookie('refresh_token_member', refreshToken, { ...cookieOptions, maxAge: 30 * 24 * 60 * 60 * 1000 })
+  if (process.env.BUILD_MODE === 'prod') {
+    if (user.role === 'admin') {
+      res.cookie('access_token_admin', accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 7 * 24 * 60 * 60 * 1000
+      })
+      res.cookie('refresh_token_admin', refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 30 * 24 * 60 * 60 * 1000
+      })
+    } else if (user.role === 'member') {
+      res.cookie('access_token_member', accessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 7 * 24 * 60 * 60 * 1000
+      })
+      res.cookie('refresh_token_member', refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        maxAge: 30 * 24 * 60 * 60 * 1000
+      })
+    }
+  } else if (process.env.BUILD_MODE === 'dev') {
+    if (user.role === 'admin') {
+      res.cookie('access_token_admin', accessToken, {
+        httpOnly: false,
+        maxAge: 7 * 24 * 60 * 60 * 1000
+      })
+      res.cookie('refresh_token_admin', refreshToken, {
+        httpOnly: false,
+        maxAge: 30 * 24 * 60 * 60 * 1000
+      })
+    } else if (user.role === 'member') {
+      res.cookie('access_token_member', accessToken, {
+        httpOnly: false,
+        maxAge: 7 * 24 * 60 * 60 * 1000
+      })
+      res.cookie('refresh_token_member', refreshToken, {
+        httpOnly: false,
+        maxAge: 30 * 24 * 60 * 60 * 1000
+      })
+    }
   }
 }
 
