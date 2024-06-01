b78e6cb21412919eb2b2b77d99e3cffc22ec06d8
tvanlong <longvan1173@gmail.com>
2024-06-01 12:36:26 +0700

    chore: Update CORS configuration to allow all domains in dev mode

--- src/configs/cors.js
+++ src/configs/cors.js
@@ -7,6 +7,8 @@ const WHITELIST_DOMAINS = [process.env.URL_CLIENT_DEPLOY, process.env.URL_ADMIN_
 // Cấu hình CORS Option
 export const corsOptions = {
   origin: function (origin, callback) {
+    console.log(`Origin: ${origin}`) // Debugging line
+
     // Nếu đang ở trong môi trường dev thì cho phép tất cả các domain
     if (process.env.BUILD_MODE === 'dev') {
       return callback(null, true)
@@ -18,7 +20,7 @@ export const corsOptions = {
     }
 
     // Cuối cùng nếu domain không được chấp nhận thì trả về lỗi
-    return callback(new Error('Not allowed by CORS'))
+    return callback(new Error())
   },
 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
