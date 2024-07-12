9e5772c251c1fda79f38bed29b27d010778f80c9
tvanlong <longvan1173@gmail.com>
2024-07-12 12:46:27 +0700

    refactor: Update order routes to remove unnecessary permission checks

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -6,17 +6,13 @@ const routerOrder = Router()
 
 // Payment with Momo
 routerOrder.post('/pay-with-momo/:userId', checkPermission('member'), orderController.createPaymentWithMomo)
-routerOrder.post('/complete-pay-with-momo', checkPermission('member'), orderController.completePaymentWithMomo)
-routerOrder.post('/transaction-status-momo', checkPermission('member'), orderController.receiveTransactionStatusMomo)
+routerOrder.post('/complete-pay-with-momo', orderController.completePaymentWithMomo)
+routerOrder.post('/transaction-status-momo', orderController.receiveTransactionStatusMomo)
 
 // Payment with ZaloPay
 routerOrder.post('/pay-with-zalopay/:userId', checkPermission('member'), orderController.createPaymentWithZaloPay)
-routerOrder.post('/complete-pay-with-zalopay', checkPermission('member'), orderController.completePaymentWithZaloPay)
-routerOrder.post(
-  '/transaction-status-zalopay',
-  checkPermission('member'),
-  orderController.receiveTransactionStatusZaloPay
-)
+routerOrder.post('/complete-pay-with-zalopay', orderController.completePaymentWithZaloPay)
+routerOrder.post('/transaction-status-zalopay', orderController.receiveTransactionStatusZaloPay)
 
 routerOrder.get('/', checkPermission('admin', 'staff'), orderController.getAllOrders)
 routerOrder.post('/:userId', checkPermission('member'), orderController.createOrderCheckout)
